1fcd50ad81856dd931170243ff72aa7103ab2dc1
Maninani93 <javvadinani422@gmail.com>
2025-03-03 02:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+let squares = document.querySelectorAll(".square");
+
+function onEnter(event) {
+	squares.forEach(square => {
+		if (square !== event.target) {
+			square.style.backgroundColor = "#6F4E37"; 
+		}
+	});
+}
+
+function onLeave() {
+	squares.forEach(square => {
+		square.style.backgroundColor = "#E6E6FA";
+	});
+}
+
+squares.forEach(square => {
+	square.addEventListener("mouseover", onEnter);
+	square.addEventListener("mouseout", onLeave);
+});
